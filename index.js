5d21fbfc4cf74061fa8079345cc8e43680c6dc94
edpuzino <edpuzino@yahoo.com>
2018-10-17 18:05:57 -0700

    bitmap so far

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const fs = require('fs');
+const greyScale = require('./lib/greyscale.js');
+
 
 /**
  * Bitmap -- receives a file name, used in the transformer to note the new buffer
@@ -9,6 +11,7 @@ const fs = require('fs');
  */
 function Bitmap(filePath) {
   this.file = filePath;
+
 }
 
 /**
@@ -16,18 +19,39 @@ function Bitmap(filePath) {
  * @param buffer
  */
 Bitmap.prototype.parse = function(buffer) {
+  this.buffer = Buffer.from(buffer);
   this.type = buffer.toString('utf-8', 0, 2);
-  //... and so on
+  console.log('type', this.type);
+  this.fileSize = buffer.readInt32LE(2); //read 32 bytes skipping the first two
+  console.log('file size', this.fileSize);
+  this.bytesPerPixel = buffer.readInt16LE(28);
+  console.log('bits per pixel', this.bytesPerPixel);
+  this.height = buffer.readInt32LE(22);
+  console.log('height', this.height);
+  this.width = buffer.readInt32LE(18);
+  console.log('width', this.width);
+  this.numberColors = buffer.readInt32LE(46);
+  console.log('number of colors', this.numberColors);
+  this.impColors = buffer.readInt32LE(50);
+  console.log('important colors', this.impColors);
+  this.pixelArrayOffset = buffer.readInt32LE(10);
+  this.rawPixels = buffer.slice(this.pixelArrayOffset);
+  this.headers = buffer.slice(0, this.pixelArrayOffset);
 };
 
 /**
  * Transform a bitmap using some set of rules. The operation points to some function, which will operate on a bitmap instance
  * @param operation
- */
+*/
+
 Bitmap.prototype.transform = function(operation) {
-  // This is really assumptive and unsafe
   transforms[operation](this);
-  this.newFile = this.file.replace(/\.bmp/, `.${operation}.bmp`);
+  //this.newFile = this.file.replace(/\.bmp/, `.${operation}.bmp`);
+
+
+
+
+
 };
 
 /**
@@ -37,21 +61,83 @@ Bitmap.prototype.transform = function(operation) {
  * @param bmp
  */
 const transformGreyscale = (bmp) => {
-
+  //console.log(bmp);
+  //Bitmap.transform('greyscale');
+  bitmap.newFile = bitmap.file.replace(/\.bmp/, `.${operation}.bmp`);
   console.log('Transforming bitmap into greyscale', bmp);
 
-  //TODO: Figure out a way to validate that the bmp instance is actually valid before trying to transform it
+  let pictureData = bitmap.rawPixels;
+  console.log(pictureData.length);
+  console.log(pictureData[1275]);
+
+  //greyscale picture
+  for(let i = 0; i < pictureData.length ; i += 3)  {      
+    let avg = (pictureData[i] + pictureData[i+1] + pictureData[i+2])/3;
+    pictureData[i] = avg;
+    pictureData[i+1] = avg;
+    pictureData[i+2] = avg;
+  }
+
+  console.log(pictureData[1275]);
+  let newPic = bitmap.headers + pictureData;
+  fs.writeFile(bitmap.newFile, newPic, (err, out) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`Bitmap Transformed: ${bitmap.newFile}`);
+  });
+};
+  //TODONE: Figure out a way to validate that the bmp instance is actually valid before trying to transform it
 
   //TODO: alter bmp to make the image greyscale ...
 
+
+
+const transformNegative = (bmp) => {
+
+  bitmap.newFile = bitmap.file.replace(/\.bmp/, `.${operation}.bmp`);
+  console.log('Transforming bitmap into an inverse color image', bmp);
+
+  let pictureData = bitmap.rawPixels;
+  console.log(pictureData.length);
+  console.log(pictureData[1275]);
+
+  for(let i = 0; i < 125; i++) {
+    for(let j = 0; j < 112; j++){
+      if(j<110) {
+        pictureData[(i*112)+j] = 255 - pictureData[(i*112)+j];
+      }
+    }
+  }
+
+    //invert picture
+   // for(let i=0; i<pictureData.length; i++) {
+        //pictureData[i] = 255 - pictureData[i];
+    //}
+    
+   
+
+    console.log(pictureData[1275]);
+    let newPic = bitmap.headers + pictureData;
+    fs.writeFile(bitmap.newFile, newPic, (err, out) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`Bitmap Transformed: ${bitmap.newFile}`);
+  });
 };
 
+
+
+//console.log(process.argv);
 /**
  * A dictionary of transformations
  * Each property represents a transformation that someone could enter on the command line and then a function that would be called on the bitmap to do this job
  */
 const transforms = {
-  greyscale: transformGreyscale
+  greyscale: transformGreyscale,
+  negative: transformNegative
+
 };
 
 // ------------------ GET TO WORK ------------------- //
@@ -65,17 +151,11 @@ function transformWithCallbacks() {
     }
 
     bitmap.parse(buffer);
-
     bitmap.transform(operation);
 
     // Note that this has to be nested!
     // Also, it uses the bitmap's instance properties for the name and thew new buffer
-    fs.writeFile(bitmap.newFile, bitmap.buffer, (err, out) => {
-      if (err) {
-        throw err;
-      }
-      console.log(`Bitmap Transformed: ${bitmap.newFile}`);
-    });
+    
 
   });
 }
